221f716877e099e433fafbb5a4e7985d60bce41e
RamanChola <ramanchola3@gmail.com>
2021-09-02 16:41:46 +0530

    cors update

--- index.js
+++ index.js
@@ -19,13 +19,7 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-const corsOptions = {
-  origin: "https://netflixfrontend-4c1f8.web.app/,http://localhost:4200/",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
