9af9b23132f1bd84cd0eee5cac4aeef4a732924e
RamanChola <ramanchola3@gmail.com>
2021-09-02 13:34:46 +0530

    changing corsagain

--- index.js
+++ index.js
@@ -19,7 +19,14 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-app.use(cors());
+const corsOptions = {
+  origin:
+    "https://netflixfrontend-4c1f8.web.app/register",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
