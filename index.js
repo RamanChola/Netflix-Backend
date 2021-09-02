e8ccd0dd916deaefbf52fa7726faca4cc86e60bb
RamanChola <ramanchola3@gmail.com>
2021-09-02 13:08:40 +0530

    changing cors

--- index.js
+++ index.js
@@ -19,14 +19,7 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-const corsOptions = {
-  origin:
-    "netflixfrontend-4c1f8.web.app,netflixfrontend-4c1f8.firebaseapp.com,https://sad-ardinghelli-a2c7cb.netlify.app",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
